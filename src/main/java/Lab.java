ea1f753ca745701b58f97eff5434ffa0e3ae612a
Brian Yang <yangbri1@msu.edu>
2024-12-31 08:46:36 +0000

    successfully create a JDBC connection, create/write desired SQL query statement (SELECT * FROM songs), execute SQL (stmt.executeQuery(sql)), & retrieve the results (each row/record) from 'song' database --- append to an ArrayList collection to display

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -38,13 +38,41 @@ public class Lab {
 
     public void createSong(Song song)  {
         //write jdbc code here
+        // INSERT INTO songs (id, title, artist) VALUES (4, 'After LIKE', 'IVE');
+        // INSERT INTO songs (song.id, title, artist) VALUES song;
+
     }
 
     public List<Song> getAllSongs(){
         List<Song> songs = new ArrayList<>();
 
         //write jdbc code here
+        
+        // creating a DB connection
+        try {
+            Connection conn = ConnectionUtil.getConnection();
+            // 
+            Statement stmt = conn.createStatement();
+            // write out desired SQL query statement
+            String sql = "SELECT * FROM songs";
+            // retrieving data from DB
+            ResultSet resultSet = stmt.executeQuery(sql);
+            // ResultSet .next() navigate method -- moves cursor to next row/record in result set, if exists return true (Note: Rows in SQL start at 1)
+            while(resultSet.next()){
+                // retrieves values from respective columns via ResultSet's .getInt(), .getString() methods
+                int id = resultSet.getInt("id");
+                String title = resultSet.getString("title");
+                String artist = resultSet.getString("artist");
 
+                // append return values to empty ArrayList declared above
+                songs.add(new Song(id, title, artist));
+            }
+            // catch any Exceptions that are thrown
+        } catch (SQLException e) {
+            // TODO: handle exception
+            e.printStackTrace();
+        }
+        // return whole 'songs' DB table
         return songs;
     }
 }
